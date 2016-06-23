e0f6f61f6ecde9f8e77911cdcd7cfc51340610a9
KC Erb <iamkcerb@gmail.com>
2016-06-23 14:15:55 -0600

    version bump

--- scripts/unlink.js
+++ scripts/unlink.js
@@ -1,4 +1,2 @@
 //Create symlink that points npm-named directory to actual MATLAB package
-fs = require('fs');
-fs.unlinkSync('../MatPack');
-fs.renameSync('../+MatPack', '../MatPack');
+console.log('Hey I am Uninstalling A thing!!')
